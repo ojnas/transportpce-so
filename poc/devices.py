402827062f4bb3fd6e28c238a9c660bd1af09681
Jonas Mårtensson <jonas.martensson@ri.se>
2020-05-27 09:27:43 +0200

    Save netconf traces from docker devices

--- poc/devices.py
+++ poc/devices.py
@@ -4,11 +4,13 @@ import docker
                         
 class DockerDevices():
     
-    started_containers = []
+    def __init__(self, devices):
+        self.devices = devices
+        self.started_containers = []
     
-    def start(self, devices):
+    def start(self):
         client = docker.from_env()
-        for dev in devices:
+        for dev in self.devices:
             self.started_containers.append(client.containers.run("confd-openroadm", dev, init=True, detach=True, tty=True, remove=True))
 
     def get_ip_addrs(self):
@@ -20,3 +22,10 @@ class DockerDevices():
         for container in self.started_containers:
             container.stop()
         self.started_containers = []
+        
+    def save_traces(self):
+        for container, dev in zip(self.started_containers, self.devices):
+            bits, stat = container.get_archive('/confd/var/confd/log/netconf.trace')
+            with open(dev+'.tar', 'wb') as f:
+                for chunk in bits:
+                    f.write(chunk)
